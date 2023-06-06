2ab7508e9f35a4da697f2790fb9d987ba3b96521
Kara Peoples <karapeoples.dev@gmail.com>
2023-06-06 08:01:06 -0500

    Working Solution

--- interactive.js
+++ interactive.js
@@ -1,40 +1,59 @@
-let star = 0
+let star
 const idOne = document.getElementById("one")
 const idTwo = document.getElementById("two")
 const idThree = document.getElementById("three")
 const idFour = document.getElementById("four")
 const idFive = document.getElementById("five")
 
-const chooseStar = (number) => {
-    switch(document.getElementById(number)){
-        case number == "one":
-        idOne.setAttribute('id', 'chosen-star')
+const chooseStarOne = () => {
         star = 1
-        break;
-        case number === "two":
-            idTwo.setAttribute('id', 'chosen-star')
-            star = 2
-            break;
-        case number === "three":
-            idThree.setAttribute('id', 'chosen-star')
-            star = 3
-            break;
-        case number === "four":
-            idFour.setAttribute('id', 'chosen-star')
-            star = 4
-            break;
-        case number === "five":
-            idFive.setAttribute('id', 'chosen-star')
-            star =  5 
-            break;          
-    }
+        idOne.setAttribute('id', "chosen-star") 
+        idTwo.setAttribute("id", 'two')
+        idThree.setAttribute('id', 'three')
+        idFour.setAttribute("id", "four")
+        idFive.setAttribute("id", "five")
+    
+}
+const chooseStarTwo = () => {
+    star = 2
+    idTwo.setAttribute('id', "chosen-star")
+    idOne.setAttribute("id", 'one')
+        idThree.setAttribute('id', 'three')
+        idFour.setAttribute("id", "four")
+        idFive.setAttribute("id", "five") 
+}
+const chooseStarThree = () => {
+    star = 3
+    idThree.setAttribute('id', "chosen-star")
+    idTwo.setAttribute("id", 'two')
+        idOne.setAttribute('id', 'one')
+        idFour.setAttribute("id", "four")
+        idFive.setAttribute("id", "five") 
+
+}
+const chooseStarFour = () => {
+    star = 4
+    idFour.setAttribute('id', "chosen-star")
+    idTwo.setAttribute("id", 'two')
+    idThree.setAttribute("id", "three")
+        idOne.setAttribute('id', 'one')
+        idFive.setAttribute("id", "five") 
+
 }
 
+const chooseStarFive = () => {
+    star = 5
+    idFive.setAttribute('id', "chosen-star")
+    idTwo.setAttribute("id", 'two')
+    idThree.setAttribute("id", "three")
+    idFour.setAttribute("id", "four") 
+    idOne.setAttribute('id', 'one')
 
-
+}
 const onClick = () => {
-
-    document.getElementById("announcement").textContent = `You selected ${star} out of 5`
+document.getElementById('rating-state-main').style.display = "none"
+document.getElementById("thank-you-state-main").style.display = "block"
+document.getElementById("announcement").textContent = `You selected ${star} out of 5`
 }
 
 
